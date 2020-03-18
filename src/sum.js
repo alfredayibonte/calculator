5172a0c89ad85f385822ccd3520c0f42f2a1ec92
alfredayibonte <alfredayibonte@gmail.com>
2020-03-18 10:00:05 +0000

    update sum function to pass test2 and 3

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,8 @@
-const sum = (a, b) => a + b;
+const sum = (a, b, ...others) => {
+  return [a, b, ...others]
+    .map((n) => parseInt(n, 10))
+    .filter((n) => !Number.isNaN(n))
+    .reduce((total, number) => total + number, 0);
+}
 
 export default sum;
