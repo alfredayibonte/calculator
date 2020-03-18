137a7818cf2f34d0d42a1116a1a62bd6da19f963
alfredayibonte <alfredayibonte@gmail.com>
2020-03-18 10:03:44 +0000

    fix linter issue

--- src/sum.js
+++ src/sum.js
@@ -1,8 +1,6 @@
-const sum = (a, b, ...others) => {
-  return [a, b, ...others]
-    .map((n) => parseInt(n, 10))
-    .filter((n) => !Number.isNaN(n))
-    .reduce((total, number) => total + number, 0);
-}
+const sum = (a, b, ...others) => [a, b, ...others]
+  .map((n) => parseInt(n, 10))
+  .filter((n) => !Number.isNaN(n))
+  .reduce((total, number) => total + number, 0);
 
 export default sum;
